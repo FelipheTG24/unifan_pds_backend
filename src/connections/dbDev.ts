f28acd7b31ff5ad91c68eb04f09b45267c8b4eca
FelipheTG24 <feliphaogomes@gmail.com>
2024-11-04 20:59:39 -0300

    atualizador

--- src/connections/dbDev.ts
+++ src/connections/dbDev.ts
@@ -1,12 +1,25 @@
 import { DataSource } from "typeorm";
 import { Task } from "../models/task";
 
+// export const DevDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "postgres",
+//     database: "backend",
+//     entities: [Task]
+// })
+
 export const DevDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "dpg-cskku0ij1k6c73bj5kn0-a.oregon-postgres.render.com",
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "backend",
-    entities: [Task]
+    username: "unifan_pds_db_user",
+    password: "3Gyy71dvYKFZImfquab6a4OYwE2wN5dn",
+    database: "unifan_pds_db",
+    entities: [Task],
+    extra: {
+        options: "-c statement_timeout=7200ms -c search_parth=monsters",
+    },
 })
